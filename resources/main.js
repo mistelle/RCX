fc97c2c07100c2176c7cc9ed4887cc86757b1465
Michelle Slay <chelle.slay.215@gmail.com>
2018-04-22 20:37:47 -0500

    trying to figure out while this doesn't work ;.;

---
+++ resources/main.js
@@ -0,0 +1,17 @@
+/*global
+	$
+*/
+
+function show() {
+	"use strict";
+	
+	$("#main").show();
+	$("#game").show();
+}
+
+function hide() {
+	"use strict";
+	
+	$("#main").hide();
+	$("#game").hide();
+}
